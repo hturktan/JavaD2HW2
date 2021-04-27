552e250fa2648d8401b3ea25fbedacb0efcea184
hturktan <82947367+hturktan@users.noreply.github.com>
2021-04-27 21:50:24 +0300

    methods

--- JavaD2HW2/src/JavaD2HW2/Methods.java
+++ JavaD2HW2/src/JavaD2HW2/Methods.java
@@ -7,9 +7,11 @@ public class Methods {
 		String mesaj = "Bugün have çok güzel";
 		String yeniMesaj = mesaj.substring(0,2); // sehirVer() kullanýlabilirdi. Çünkü ikisi de string.
 		System.out.println(yeniMesaj);
-		int sayi = topla(5, 7); // topla() kullanýlabilmesinin nedeni int deðer dönmesinden.
+		int sayi = topla(15, 7); // topla() kullanýlabilmesinin nedeni int deðer dönmesinden.
 							// toplama yaptýrmak için iki parametre vermek gerek. Toplama iþlemi yapmaz. 
 		System.out.println(sayi);
+		int toplam = topla2(2,3,4,5,6); // Sayýlarý diziye çaviriyor. 
+		System.out.println(toplam);
 	}
 
 	public static void ekle() {
@@ -28,6 +30,16 @@ public class Methods {
 	public static int topla(int sayi1, int sayi2) { // int türünde deðer döndürür - Toplama için topla() içine int deðerler verilmeli.
 		//return 5; // burada 5 döndür demek. - Parametre verildikten sonra da sayi1 + sayi2 yazýlýr.
 		return sayi1+sayi2;
+	}
+	
+	public static int topla2(int... sayilar) { // int den sonra 3 tane nokta ekledil. Variable Arguments deniyor. 
+
+		int toplam = 0;
+		for (int sayi : sayilar) {
+			toplam += sayi;
+		}
+		
+		return toplam;
 	}
 	
 	public static String sehirVer() {
