0bd1d38f3fc2300611a533ea7543367c2c75202e
hturktan <82947367+hturktan@users.noreply.github.com>
2021-04-27 20:57:15 +0300

    main

--- JavaD2HW2/src/JavaD2HW2/Main.java
+++ JavaD2HW2/src/JavaD2HW2/Main.java
@@ -3,8 +3,34 @@ package JavaD2HW2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 sayiBulmaca();
+		
+	}
+	
+	public static void sayiBulmaca() {
+		
+		int[] sayilar = new int[]{1,2,5,7,9,0};
+        int aranacak = 5;
+        boolean varMi = false;
 
+        for(int sayi: sayilar){
+            if(sayi == aranacak){
+                varMi = true;
+                break;
+            }
+        }
+        String mesaj = "";
+        if(varMi){
+        	mesaj = "Sayý mevcuttur: "+aranacak;
+            mesajVer(mesaj);
+        }else{
+            System.out.println("Sayý mevcut deðildir");
+        }
+        
 	}
 
+	public static void mesajVer(String mesaj) { // parametreli fonksiyon. 
+		System.out.println(mesaj);
+	}
+	
 }
