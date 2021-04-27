56c5f32b388efbb25b3302e70027c8a29f979568
hturktan <82947367+hturktan@users.noreply.github.com>
2021-04-28 00:57:03 +0300

    HW2

---
+++ JavaD2HW2/src/JavaD2HW2/DortIslem2.java
@@ -0,0 +1,15 @@
+package JavaD2HW2;
+
+public class DortIslem2 {
+	public int Topla(int sayi1, int sayi2) {
+		return sayi1+sayi2;
+	}
+	
+	// ikisine de topla dersek hata olmaz. Parametrelerinin imzası farklı. 
+	// İlkinde int int istiyor, ikincisinde int int int gerekiyor.
+	// Buna da overloading deniyor.  
+	
+	public int Topla2(int sayi1, int sayi2, int sayi3) {
+		return sayi1+sayi2;
+	}
+}
